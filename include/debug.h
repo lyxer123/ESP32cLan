75dd982f5abcfe0c0d30b362e22a1b9b3ac5c436
lyxer123 <42857073+lyxer123@users.noreply.github.com>
2024-05-30 18:00:35 +0800

    加入debug.h文件

---
+++ include/debug.h
@@ -0,0 +1,35 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#include <Arduino.h>
+#include "myVar.h"
+#include "interpreter.h"
+
+#ifndef NO_DEBUGGER
+
+/* Constants */
+#define BREAKPOINT_TABLE_SIZE 21
+#define BREAKPOINT_HASH(p) ( ((unsigned long)(p)->FileName) ^ (((p)->Line << 16) | (0 << 16)) )
+
+/* Global variables */
+extern char SingleStep;
+extern char DebugBar[];
+extern char sendChunked;
+extern struct Table BreakpointTable;
+extern struct TableEntry *BreakpointHashTable[BREAKPOINT_TABLE_SIZE];
+extern int BreakpointCount;
+extern int DebugManualBreak;
+
+/* Function declarations */
+void DebugInit(void);
+void DebugCleanup(void);
+void DebugSetBreakpoint(struct ParseState *Parser);
+int DebugClearBreakpoint(struct ParseState *Parser);
+void DebugCheckStatement(struct ParseState *Parser);
+void PrintValue(struct Value *Var);
+int DebugStep(struct ParseState *Parser);
+void prntLines(struct ParseState *Parser);
+
+#endif /* !NO_DEBUGGER */
+
+#endif /* DEBUG_H */
